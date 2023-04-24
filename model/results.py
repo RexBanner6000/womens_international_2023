37519cd92f9129e40edfb0a679129a76a22eddbb
RexBanner6000 <tghaynes@gmail.com>
2023-04-24 07:33:20 +0100

    Add data from matches

--- model/results.py
+++ model/results.py
@@ -15,9 +15,7 @@ class ResultsDataset:
     teams: List[Team] = field(default_factory=list)
 
     def populate_data_from_df(self, df: DataFrame) -> None:
-        self._get_teams_from_df(df)
         self._get_events_from_df(df)
-        self._get_tournaments_from_df(df)
         self._get_matches_from_df(df)
 
     def get_event_from_name(self, event_name: str) -> Optional[Event]:
@@ -40,15 +38,14 @@ class ResultsDataset:
                 return tournament
         return None
 
-    def _get_teams_from_df(self, df: DataFrame) -> None:
-        for index, row in df.iterrows():
-            self._add_team(row["home_team"])
-            self._add_team(row["away_team"])
-
-    def _add_team(self, team_name: str) -> None:
+    def _create_team(self, team_name: str) -> Team:
         team_names = [x.name for x in self.teams]
         if team_name not in team_names:
-            self.teams.append(Team(name=team_name))
+            team = Team(name=team_name)
+            self.teams.append(team)
+        else:
+            team = self.get_team_from_name(team_name)
+        return team
 
     def _get_events_from_df(self, df: DataFrame) -> None:
         for index, row in df.iterrows():
@@ -59,32 +56,27 @@ class ResultsDataset:
         if event_name not in event_names:
             self.events.append(Event(name=event_name))
 
-    def _get_tournaments_from_df(self, df: DataFrame) -> None:
-        for index, row in df.iterrows():
-            year = datetime.strptime(row["date"], "%Y-%m-%d").year
-            event_name = row["tournament"]
-            self._add_event(event_name)
-            self._add_tournament(event_name, year)
-
-    def _add_tournament(self, event_name: str, year: int) -> None:
+    def _create_tournament(self, event_name: str, year: int) -> Tournament:
         event = self.get_event_from_name(event_name)
         for tournament in self.tournaments:
             if tournament.name == event.name and tournament.year == year:
-                return None
+                return tournament
 
-        self.tournaments.append(Tournament(event_name, year))
+        tournament = Tournament(event_name, year)
+        self.tournaments.append(tournament)
+        return tournament
 
     def _get_matches_from_df(self, df: DataFrame) -> None:
         for index, row in df.iterrows():
             date = datetime.strptime(row["date"], "%Y-%m-%d")
             self.matches.append(
                 Match(
-                    home_team=self.get_team_from_name(row["home_team"]),
-                    away_team=self.get_team_from_name(row["away_team"]),
+                    home_team=self._create_team(row["home_team"]),
+                    away_team=self._create_team(row["away_team"]),
                     date=date,
                     home_score=row["home_score"],
                     away_score=row["away_score"],
-                    tournament=self.get_tournament_from_year(
+                    tournament=self._create_tournament(
                         row["tournament"], date.year
                     ),
                     city=row["city"],
