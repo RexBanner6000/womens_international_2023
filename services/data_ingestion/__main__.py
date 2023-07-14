a558f8363ba215acb3e14971397b1a923fe26a7e
RexBanner6000 <tghaynes@gmail.com>
2023-07-14 09:25:33 +0100

    Remove unnecessary import

--- services/data_ingestion/__main__.py
+++ services/data_ingestion/__main__.py
@@ -1,13 +1,11 @@
 import logging
-
 from pathlib import Path
 
 import pandas as pd
 
 from data_ingestor.ingestor import create_dataset_from_file
 
-
-raw_data = "./data/kaggle/results.csv"
+raw_data = "./data/kaggle/mens_results.csv"
 sample_submission = "./rss-wwc-2023-prediction-competition/submission-template.csv"
 
 if __name__ == "__main__":
@@ -18,10 +16,10 @@ if __name__ == "__main__":
     submission_df = pd.read_csv(sample_submission)
 
     logging.info("Generating training output...")
-    results.write_results_to_csv(Path("./results.csv"))
+    results.write_results_to_csv(Path("./results_from_mens.csv"))
 
     logging.info("Generating test output")
     test_df = results.create_test_df(submission_df)
-    test_df.to_csv("./test.csv", index=False)
+    test_df.to_csv("./test_from_mens.csv", index=False)
 
     logging.info("Done!")
