542f09d8ad9eeccad9ee0b3584e2f5206e8ee418
RexBanner6000 <tghaynes@gmail.com>
2023-07-18 21:45:55 +0100

    Added parser to data ingestion

--- services/data_ingestion/__main__.py
+++ services/data_ingestion/__main__.py
@@ -1,25 +1,50 @@
-import logging
+from argparse import ArgumentParser
 from pathlib import Path
 
 import pandas as pd
 
 from data_ingestor.ingestor import create_dataset_from_file
 
-raw_data = "./data/kaggle/results.csv"
-sample_submission = "./rss-wwc-2023-prediction-competition/submission-template.csv"
 
 if __name__ == "__main__":
-    logging.info(f"Reading data from {raw_data}...")
-    results = create_dataset_from_file(Path(raw_data))
+    parser = ArgumentParser()
+    parser.add_argument(
+        "raw_data",
+        help="Match results file to ingest",
+        type=Path
+    )
 
-    logging.info(f"Reading sample submission from {sample_submission}")
-    submission_df = pd.read_csv(sample_submission)
+    parser.add_argument(
+        "sample_submission",
+        help="Sample submission file",
+        type=Path
+    )
 
-    logging.info("Generating training output...")
-    results.write_results_to_csv(Path("./results_from_mens.csv"))
+    parser.add_argument(
+        "--training_output",
+        help="File name for training output",
+        default="training_data.csv"
+    )
 
-    logging.info("Generating test output")
+    parser.add_argument(
+        "--submission_output",
+        help="File name for submission output",
+        default="submission_data.csv"
+    )
+
+    args = parser.parse_args()
+    
+    print(f"Reading data from {args.raw_data}...")
+    results = create_dataset_from_file(Path(args.raw_data))
+
+    print(f"Reading sample submission from {args.sample_submission}")
+    submission_df = pd.read_csv(args.sample_submission)
+
+    print(f"Generating training output {args.training_output}...")
+    results.write_results_to_csv(Path(args.training_output))
+
+    print(f"Generating test output {args.submission_output}...")
     test_df = results.create_test_df(submission_df)
-    test_df.to_csv("./test_from_mens.csv", index=False)
+    test_df.to_csv(args.submission_output, index=False)
 
-    logging.info("Done!")
+    print("Done!")
