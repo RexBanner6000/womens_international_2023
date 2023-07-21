8a7f103304229002c8c8e57165d5495f37cd6398
RexBanner6000 <tghaynes@gmail.com>
2023-07-21 11:08:08 +0100

    Connect to DB

--- services/scraper/__main__.py
+++ services/scraper/__main__.py
@@ -1,8 +1,15 @@
-import selenium
+import os
 
+import psycopg2
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+
+
+def connect_to_db():
+    conn = psycopg2.connect(os.environ["DATABASE_URL"])
+    return conn
+
 
 if __name__ == "__main__":
     url = "https://www.flashscore.co.uk/football/world/world-cup-women/#/plFV0LBD/live"
@@ -11,6 +18,8 @@ if __name__ == "__main__":
     summary = driver.find_elements(By.CLASS_NAME, "event--summary")
     matches = summary[0].find_elements(By.CLASS_NAME, "event__match--twoLine")
 
+    conn = connect_to_db()
+
     for match in matches:
         try:
             home_score = match.find_element(By.CLASS_NAME, "event__score--home")
@@ -18,7 +27,9 @@ if __name__ == "__main__":
             event_stage = match.find_element(By.CLASS_NAME, "event__stage")
             home_team = match.find_element(By.CLASS_NAME, "event__participant--home")
             away_team = match.find_element(By.CLASS_NAME, "event__participant--away")
-            print(f"{home_team.text}: {home_score.text} {away_team.text}: {away_score.text} - Status: {event_stage.text}")
+            print(
+                f"{home_team.text}: {home_score.text} {away_team.text}: {away_score.text} - Status: {event_stage.text}"
+            )
         except NoSuchElementException:
             continue
 
